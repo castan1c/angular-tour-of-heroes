466282a48625ad501a3cff68d5fc6d93401131ca
vantonov <vantonov@griddynamics.com>
2019-10-03 18:13:06 +0300

    Tour of heroes 4 sections

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Mira' },
+    { id: 12, name: 'Ira' },
+    { id: 13, name: 'Misha' },
+    { id: 14, name: 'Andriy' },
+    { id: 15, name: 'Dima' },
+    { id: 16, name: 'Olena' },
+    { id: 17, name: 'Khrystyna' },
+    { id: 18, name: 'Vasyl' },
+    { id: 19, name: 'Khrystyna 2' },
+    { id: 20, name: 'Vlad' }
+];
